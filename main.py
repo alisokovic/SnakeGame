0651b0d6b22e869c8c878fa88f2b49db825c74be
alisokovic <alisokovic@users.noreply.github.com>
2023-11-18 16:45:48 +0300

    snake's moving algorithm is added

--- main.py
+++ main.py
@@ -18,26 +18,32 @@ class FRUIT:
 
 class SNAKE:
     def __init__(self):
-        self.color = (155,35,57)
+        self.color = (155, 35, 57)
         self.body = [Vector2(10, 9), Vector2(9, 9), Vector2(8, 9)]
+        self.direction = Vector2(1, 0)
 
     def draw_snake(self):
         for block in self.body:
             block_rect = pygame.Rect((block.x * cell_size), (block.y * cell_size), cell_size, cell_size)
             pygame.draw.rect(screen, self.color, block_rect)
-            
-            
-            
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy
+
+
 class MAIN:
     def __init__(self):
         self.snake = SNAKE()
         self.fruit = FRUIT()
-        
+
     def draw_elements(self):
         self.snake.draw_snake()
         self.fruit.draw_fruit()
-        
 
+    def update(self):
+        self.snake.move_snake()
 
 
 pygame.init()
@@ -46,20 +52,25 @@ cell_size = 60
 cell_col_number = 30
 cell_row_number = 15
 screen_size = (int(cell_col_number * cell_size), int(cell_row_number * cell_size))
-screen_color = (35, 188, 72)
+screen_color = (35, 170, 72)
 
 screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
 
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+
 
 main_game = MAIN()
-
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
 
     screen.fill(screen_color)
 
